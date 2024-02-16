5025c9fc0150ea40380289c8cda149de0289ef19
Abraham Osco <aosco@fi.uba.ar>
2024-02-16 18:06:16 -0300

    subo avance del tres rayas

--- Projects/tresEnRaya/README.md
+++ Projects/tresEnRaya/README.md
@@ -1,2 +1,56 @@
 # Juego tres en raya 🤯 :facepunch:
 
+1. grid-template-columns: repeat(3, 1fr);  3 columnas por fraccion.
+1. En javascript:
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const newBoard = [...board]
+  newBoard[index] = turn
+  setBoard(newBoard)
+  Usar const es prohibir la reasignacion pero eso no impide que podamos cambiar los valores
+  a los elementos en el caso de que newBoard sea un array o un objeto. **se puede cambiar**
+1. No hacemos directamente: 
+    board[index] = turn
+    setBoard(board).
+    ESTO ESTA MAL ❌ No tenemo que modificar nunca las props de las componentes ni los estados
+    que tenemos en el useState.
+    Estamos modificando el estado actual sin llamar a setBoard() ❌. 
+
+    Siempre debemos crear un nuevo array con el valor en este caso. (lo hacemos con el spread)
+1. con el spread operator '...' realizamos una copia superficial.
+1. con structuredClone(board) podemos hacer un deep copy. 
+1. Para los useState lo podemos asociar a unos enums (pseudoEnum).
+1. Hay 3 tipos de fors en javascript:
+    1. for (let i = 0; i < cars.length; i++) {
+            text += cars[i] + "<br>";
+        }
+    1. for in se usa para iterar sobre las propiedad de un objeto.
+        const person = {fname:"John", lname:"Doe", age:25};
+        let text = "";
+        for (let x in person) {
+            text += person[x];  
+        } // John Doe 25
+        tambien podemos iterar los elementos un array: 
+        const numbers = [45, 4, 9, 16, 25];
+        let txt = "";
+        for (let x in numbers) {
+            txt += numbers[x];
+        }// 45 4 9 16 25
+    1. for of se usa para iterar todos los valores sobre un objeto iterable. jaja
+        const cars = ["BMW", "Volvo", "Mini"];
+        let text = "";
+        for (let x of cars) {
+            text += x;
+        }
+1. Actualizacion de los estados en react es asincrono. 
+1. Si usariamos board en checkWinner alguien podria pensar que ya tiene el estado nuevo pero no es asi. 
+1. Dentro del useState podemos crear un arrow function con esta jugada podemos tener acceso al estado
+   anterior y al nuevo:
+    if (newWinner == 'W'){
+      setWinner( (previewState) => {
+        console.log(`Estado Actual ${newWinner} y el anterior era ${previewState}`)
+        return newWinner
+      })
+    } 
+1. No podes hacer un async await porque esto no devuelve una promesa. 
+1.  
+
--- Projects/tresEnRaya/src/App.jsx
+++ Projects/tresEnRaya/src/App.jsx
@@ -1,34 +1,114 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X:'X',
+  O:'O'
+}
+
+const GAME_STATE = {
+  IN_GAME:'I',
+  WIN:'W',
+  DRAW:'D'
+}
+
+const WINNER_COMBO = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+
+// crearemos un componente square. 
+const Square = ( {children, isSelect, updateBoard, index} ) => {
+  const aClassName = 'square' + (isSelect ? ' is-selected' : '')
+  const handlerClick = () => {
+    updateBoard(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handlerClick} className={aClassName}>
+      {children}
+    </div>
+  )
+} 
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(GAME_STATE.IN_GAME)
+
+  const updateBoard = (index) => {
+    console.log(winner)
+    if( board[index] || winner != GAME_STATE.IN_GAME) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = (turn == TURNS.X ? TURNS.O : TURNS.X)
+    setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner == 'W'){
+      setWinner( (previewState) => {
+        console.log(`Ganador ${newWinner}, el anterior era ${previewState}`)
+        return newWinner
+      })
+    } else if (newWinner == 'D'){
+      setWinner(GAME_STATE.DRAW)
+    }
+    
+  }
+
+  const checkWinner = (aBoard) => {
+    for (const aCombo of WINNER_COMBO){
+      const [a, b, c] = aCombo
+      if (aBoard[a] && aBoard[a] == aBoard[b] && aBoard[a] == aBoard[c] ){
+        return GAME_STATE.WIN
+      }
+    }
+    let jugadas = 0
+    for (const elemento of aBoard){
+      if (elemento){
+        jugadas +=1
+      }
+    }
+    if (jugadas == 9 ){
+      return GAME_STATE.DRAW
+    } 
+    return GAME_STATE.IN_GAME
+  }
+  
+  return (
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map( (_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard} >
+                {board[index]}
+              </Square>
+            )
+          } )
+        }
+      </section>
+      <section className="turn">
+        <Square isSelect={ turn == 'X'} >
+          {TURNS.X}
+        </Square>
+        <Square isSelect={ turn == 'O'}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+    </main>
   )
 }
 
